164f1ec2c322f8a393edd1180335c0fd75918fff
tatsuya-kusumoto <tkusumot118217@gmail.com>
2024-04-03 23:47:11 +0900

    Lesson5 課題提出

--- src/baseball/BaseBallTeam.java
+++ src/baseball/BaseBallTeam.java
@@ -39,20 +39,19 @@ public class BaseBallTeam {
     }
 
     private double getRate() {
-        double winRate = this.win / (this.win + this.lose);
+        double winRate = (double) this.win / (this.win + this.lose);
         return winRate;
     }
 
-    public BaseBallTeam(String name,int win, int lose ,int draw){
+    public BaseBallTeam(String name, int win, int lose, int draw) {
         this.name = name;
         this.win = win;
         this.lose = lose;
         this.draw = draw;
     }
-    
-        
+
     public void report() {
-        System.out.println("2022年の成績は " + win + " 勝 " + lose + " 負 " + draw + " 分、勝率は" + this.getRate() + "です。");
+        System.out.println(name + " の2022年の成績は " + win + "勝 " + lose + "負 " + draw + "分、勝率は " + this.getRate() + "です。");
     }
 
 }
