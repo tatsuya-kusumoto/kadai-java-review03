855448ae5bd790cd7ef0ee31fa8fca51cd7282fb
tatsuya-kusumoto <164455573+tatsuya-kusumoto@users.noreply.github.com>
2024-04-03 16:40:58 +0900

    Update Review03.java
    第四回メンタリングにてアドバイス内容反映02

--- src/baseball/Review03.java
+++ src/baseball/Review03.java
@@ -3,7 +3,8 @@ package baseball;
 public class Review03 {
 
     public static void main(String[] args) {
-
+        BaseBallTeam team01 = new BaseBallTeam("東京ヤクルトスワローズ",80,59,4);
+        team01.report();
     }
 
 }
